a952fc03e132c626fde00077284270a68ae71b07
Vyshak Srishylappa <vyshaksrishyla@gmail.com>
2018-12-19 15:48:39 -0500

    Documentation: Added Usage Information.

--- Agero.Core.Validator.Tests/QuickStartTests.cs
+++ Agero.Core.Validator.Tests/QuickStartTests.cs
@@ -1,23 +1,53 @@
-﻿using System.Linq;
-using Agero.Core.Validator.Attributes;
+﻿using Agero.Core.Validator.Attributes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace Agero.Core.Validator.Tests
 {
-    public class MyClass
+    public class TestClass
     {
-        [IntValidate("Id must be greater than 0.", MinValue = 1)]
-        public int Id { get; set; }
+        [ByteValidate("ByteValue must have value between 1 and 100.", MinValue = 1, MaxValue = 100)]
+        public byte ByteValue { get; set; }
 
-        [StringValidate("Text must have lenghth between 2 and 100 characters.", MinLength = 2, MaxLength = 100)]
-        public string Text { get; set; }
+        [DecimalValidate("DecimalValue must have value between 2.45 and 100.98", MinValueString = "2.45", MaxValueString = "100.98")]
+        public decimal DecimalValue { get; set; }
 
-        [ComplexTypeValidate] // This attribute tells ValidationHelper to validate MyValueClass object
-        [NotNullValidate("MyValue must be specified.")]
-        public MyValueClass MyValue { get; set; }
+        [DoubleValidate("DoubleValue must have value between 2.45 and 100.98", MinValue = 2.45, MaxValue = 100.98)]
+        public double DoubleValue { get; set; }
+
+        [IntValidate("IntValue must value between 1 and 1000", MinValue = 1, MaxValue = 1000)]
+        public int IntValue { get; set; }
+
+        [LongValidate("LongValue must value between 1 and 1000", MinValue = 1, MaxValue = 1000)]
+        public long LongValue { get; set; }
+
+        [NullableDecimalValidate("NullableDecimalValue must have value between 2.45 and 100.98", MinValueString = "2.45", MaxValueString = "100.98")]
+        public decimal? NullableDecimalValue { get; set; }
+
+        [NullableDoubleValidate("NullableDoubleValue must have value between 2.45 and 100.98", MinValue = 2.45, MaxValue = 100.98)]
+        public double? NullableDoubleValue { get; set; }
+
+        [NullableIntValidate("NullableIntValue must value between 1 and 1000", MinValue = 1, MaxValue = 1000)]
+        public int? NullableIntValue { get; set; }
+
+        [NullableLongValidate("NullableLongValue must value between 1 and 1000", MinValue = 1, MaxValue = 1000)]
+        public long? NullableLongValue { get; set; }
+
+        [NullableShortValidate("NullableShortValue must value between 1 and 1000", MinValue = 1, MaxValue = 1000)]
+        public short? NullableShortValue { get; set; }
+
+        [ShortValidate("ShortValue must value between 1 and 1000", MinValue = 1, MaxValue = 1000)]
+        public short ShortValue { get; set; }
+
+        [StringValidate("StringValue must have length between 2 and 100 characters.", MinLength = 2, MaxLength = 100, CanBeNull = true, CanBeEmpty = false)]
+        public string StringValue { get; set; }
+
+        [ComplexTypeValidate] // This attribute tells ValidationHelper to validate TestValueClass object
+        [NotNullValidate("TestValue must be specified.")]
+        public TestValueClass TestValue { get; set; }
     }
 
-    public class MyValueClass
+    public class TestValueClass
     {
         [NotNullValidate("Value must be specified.")]
         public object Value { get; set; }
@@ -26,18 +56,63 @@ namespace Agero.Core.Validator.Tests
     [TestClass]
     public class QuickStartTests
     {
+        #region Validate method Usage
+        [TestMethod]
+        public void Test_Validate_Should_Return_No_Errors()
+        {
+            //Arrange
+            var testClass =
+                new TestClass
+                {
+                    ByteValue = 5, 
+                    DecimalValue = 3.5m,
+                    DoubleValue = 3.5,
+                    IntValue = 2, 
+                    LongValue = 1000, 
+                    NullableDecimalValue = null, 
+                    NullableDoubleValue = null, 
+                    NullableIntValue = null, 
+                    NullableLongValue = null,
+                    NullableShortValue = null,
+                    ShortValue = 1000,
+                    StringValue = "Some text",
+                    TestValue = 
+                        new TestValueClass
+                        {
+                            Value = "Some text"
+                        }
+                };
+
+            //Act
+            var validator = new ValidationHelper();
+            var validationErrors = validator.Validate(testClass);
+
+            //Assert
+            Assert.IsNull(validationErrors);
+        }
+
         [TestMethod]
         public void Test_Validate_Should_Return_Errors()
         {
             var myClass =
-                new MyClass
+                new TestClass
                 {
-                    Id = -1, // Invalid
-                    Text = "Some text", // Valid
-                    MyValue = // Valid
-                        new MyValueClass
+                    ByteValue = 5,
+                    DecimalValue = 3.5m,
+                    DoubleValue = 3.5,
+                    IntValue = -1, //invalid
+                    LongValue = 1000,
+                    NullableDecimalValue = null,
+                    NullableDoubleValue = null,
+                    NullableIntValue = null,
+                    NullableLongValue = null,
+                    NullableShortValue = null,
+                    ShortValue = 1000,
+                    StringValue = "Some text",
+                    TestValue =
+                        new TestValueClass
                         {
-                            Value = null // Invalid
+                            Value = null //invalid
                         }
                 };
 
@@ -48,12 +123,117 @@ namespace Agero.Core.Validator.Tests
             Assert.AreEqual(2, validationErrors.Errors.Count);
 
             var error1 = validationErrors.Errors.First();
-            Assert.AreEqual("Id", error1.Key);
-            Assert.AreEqual("Id must be greater than 0.", error1.Message);
+            Assert.AreEqual("IntValue", error1.Key);
+            Assert.AreEqual("IntValue must value between 1 and 1000", error1.Message);
 
             var error2 = validationErrors.Errors.Last();
             Assert.AreEqual("Value", error2.Key);
             Assert.AreEqual("Value must be specified.", error2.Message);
         }
+        #endregion
+
+        #region IsValid method usage
+        [TestMethod]
+        public void Test_IsValid_Should_Return_True()
+        {
+            //Arrange
+            var testClass =
+                new TestClass
+                {
+                    ByteValue = 5,
+                    DecimalValue = 3.5m,
+                    DoubleValue = 3.5,
+                    IntValue = 2,
+                    LongValue = 1000,
+                    NullableDecimalValue = null,
+                    NullableDoubleValue = null,
+                    NullableIntValue = null,
+                    NullableLongValue = null,
+                    NullableShortValue = null,
+                    ShortValue = 1000,
+                    StringValue = "Some text",
+                    TestValue =
+                        new TestValueClass
+                        {
+                            Value = "Some text"
+                        }
+                };
+
+            //Act
+            var validator = new ValidationHelper();
+            var validationErrors = validator.IsValid(testClass);
+
+            //Assert
+            Assert.IsTrue(validationErrors);
+        }
+
+        [TestMethod]
+        public void Test_IsValid_Should_Return_False()
+        {
+            //Arrange
+            var testClass =
+                new TestClass
+                {
+                    ByteValue = 5,
+                    DecimalValue = 3.5m,
+                    DoubleValue = 3.5,
+                    IntValue = 2,
+                    LongValue = 1000,
+                    NullableDecimalValue = null,
+                    NullableDoubleValue = null,
+                    NullableIntValue = null,
+                    NullableLongValue = null,
+                    NullableShortValue = null,
+                    ShortValue = 1000,
+                    StringValue = "S", //invalid
+                    TestValue =
+                        new TestValueClass
+                        {
+                            Value = null //invalid
+                        }
+                };
+
+            //Act
+            var validator = new ValidationHelper();
+            var validationErrors = validator.IsValid(testClass);
+
+            //Assert
+            Assert.IsFalse(validationErrors);
+        }
+        #endregion
+
+        #region CheckIsValid method Usage
+        [TestMethod]
+        [ExpectedException(typeof(ValidationException<Agero.Core.Validator.Tests.TestClass>))]
+        public void Test_CheckIsValid_Should_Throw_Exception()
+        {
+            //Arrange
+            var testClass =
+                new TestClass
+                {
+                    ByteValue = 5,
+                    DecimalValue = 3.5m,
+                    DoubleValue = 3.5,
+                    IntValue = 2,
+                    LongValue = 1000,
+                    NullableDecimalValue = null,
+                    NullableDoubleValue = null,
+                    NullableIntValue = null,
+                    NullableLongValue = null,
+                    NullableShortValue = null,
+                    ShortValue = 1000,
+                    StringValue = "S", //invalid
+                    TestValue =
+                        new TestValueClass
+                        {
+                            Value = null //invalid
+                        }
+                };
+
+            //Act
+            var validator = new ValidationHelper();
+            validator.CheckIsValid(testClass);
+        }
+        #endregion
     }
 }
--- README.md
+++ README.md
@@ -7,4 +7,57 @@ Model validation library for .NET applications.
 
 ## Usage:
 
-For usage information refer [QuickStartTests.cs](./Agero.Core.Validator.Tests/QuickStartTests.cs)
+Define model classes with properties having validation attributes (e.g.,**IntValidate**) having constrainsts (e.g.,**MinValue**) according to your use case. An small is shown below. For additional usage information refer [QuickStartTests](./Agero.Core.Validator.Tests/QuickStartTests.cs).
+
+```csharp
+public class MyClass
+{
+    [IntValidate("Id must be greater than 0.", MinValue = 1)]
+    public int Id { get; set; }
+
+    [StringValidate("Text must have lenghth between 2 and 100 characters.", MinLength = 2, MaxLength = 100)]
+    public string Text { get; set; }
+
+    [ComplexTypeValidate] // This attribute tells ValidationHelper to validate MyValueClass object
+    [NotNullValidate("MyValue must be specified.")]
+    public MyValueClass MyValue { get; set; }
+}
+
+public class MyValueClass
+{
+    [NotNullValidate("Value must be specified.")]
+    public object Value { get; set; }
+}
+```
+
+Create an instance of **MyClass**.
+```csharp
+var myClass =
+    new MyClass
+    {
+        Id = -1, // Invalid
+        Text = "Some text", // Valid
+        MyValue = // Valid
+            new MyValueClass
+            {
+                Value = null // Invalid
+            }
+    };
+```
+
+**Validator**, provides three methods to validate the object. 
+
+* **Validate:** Returns a list of validation errors.
+* **IsValid:** Returns a boolean, representing if the object is valid.
+* **CheckIsValid:** Throws an exception [ValidationException](./Agero.Core.Validator/ValidationException.cs) if the object is invalid.
+
+```csharp
+var validator = new ValidationHelper();
+
+var validationErrors = validator.Validate(myClass);
+
+var isMyclassValid = validator.IsValid(myClass);
+
+validator.CheckIsValid(myClass);
+```
+
